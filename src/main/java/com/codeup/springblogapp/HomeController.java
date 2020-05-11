7835b64651c51c14755a5d1f85367f8e9e83dbf5
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-05-11 14:26:16 -0500

    added demo

--- src/main/java/com/codeup/springblogapp/HomeController.java
+++ src/main/java/com/codeup/springblogapp/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 //Create a HomeController class. This class should have one method with a GetMapping for /. It should return a string that says "This is the landing page!".
 //This will eventually be the page that users see when they first visit your page. Later on, when we learn about views, you can swap it out for something more fancy.
 @Controller
