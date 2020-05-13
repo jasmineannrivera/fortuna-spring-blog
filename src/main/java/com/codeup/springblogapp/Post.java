60c68d030032b60aa72eb9dcea45609f8a0e6e95
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-05-13 14:31:20 -0500

    working through exercises

--- src/main/java/com/codeup/springblogapp/Post.java
+++ src/main/java/com/codeup/springblogapp/Post.java
@@ -1,28 +1,29 @@
 package com.codeup.springblogapp;
+import javax.persistence.*;
 
+
+@Entity
+@Table(name = "posts")
 public class Post {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length = 100)
     private String title;
+    @Column(nullable = false)
     private String body;
 
-    public Post() {}
+    public Post(){}
 
-    public Post(int id,String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
 
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Post(String title, String body, long id) {
+        this.title = title;
+        this.body = body;
         this.id = id;
     }
 
@@ -41,4 +42,17 @@ public class Post {
     public void setBody(String body) {
         this.body = body;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
+
+
+
+
+
--- src/main/java/com/codeup/springblogapp/PostController.java
+++ src/main/java/com/codeup/springblogapp/PostController.java
@@ -1,23 +1,50 @@
 package com.codeup.springblogapp;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+//        GET	/posts	posts index page
+//        GET	/posts/{id}	view an individual post
+//        GET	/posts/create	view the form for creating a post
+//        POST	/posts/create	create a new post
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
+    public String index(Model model){
+        ArrayList<Post> postList = new ArrayList<>();
 
-    public String postIndex() {
-        return "index";
+
+
+        for (int i = 1; i <= 5; i++) {
+            String fTitle = "Day " + i;
+            String fBody = "This is the body of a post for day " + i;
+            postList.add(new Post( fTitle, fBody));
+        }
+
+        model.addAttribute("posts", postList);
+
+        return "posts/index";
     }
+
     @GetMapping("/posts/{id}")
-    public String postByID(@PathVariable int id) {
-        return "show";
+    public String post(@PathVariable long id, Model model){
+        Post newPost = new Post("Example Post", "This is the body of a post!");
+        model.addAttribute("post", newPost);
+        return "posts/show";
     }
 
-
-
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String postCreateForm(){
+//        return "Viewing the form to create a post";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    @ResponseBody
+//    public String createNewPost(){
+//        return "Creating a new post";
+//    }
 }
