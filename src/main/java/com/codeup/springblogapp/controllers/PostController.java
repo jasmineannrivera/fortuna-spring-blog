65fec9c68b903a6e4092b66e8478e681bad5cbda
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-05-17 18:19:59 -0500

    added some helpful comments

--- src/main/java/com/codeup/springblogapp/controllers/PostController.java
+++ src/main/java/com/codeup/springblogapp/controllers/PostController.java
@@ -13,27 +13,27 @@ public class PostController {
     public PostController(PostRepository postDao) {
         this.postDao = postDao;
     }
+    //show all the posts
     @GetMapping("/posts")
     public String index(Model model) {
         model.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
-
-
+    //show the individual post the user clicked on
     @GetMapping("/posts/{id}")
     public String post(@PathVariable long id, Model model){
         model.addAttribute("post",postDao.getOne(id));
         return "posts/show";
     }
-
+    //show form for editing specific post
     @GetMapping("/posts/{id}/edit")
     public String editForm(@PathVariable long id, Model model) {
         Post postToEdit = postDao.getOne(id);
         model.addAttribute("post", postToEdit);
         return "posts/edit";
     }
-
+    //submit the edits and redirect to main posts page
     @PostMapping("/posts/{id}/edit")
     public String edited(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
         Post p = postDao.getOne(id);
@@ -43,19 +43,19 @@ public class PostController {
         return "redirect:/posts";
     }
 
-
+    //show the form for creating a new post
     @GetMapping("/posts/create")
     public String showCreatePostForm(Model model){
         System.out.println("posts/create method");
         model.addAttribute("post", new Post());
         return "posts/create";
     }
-
+    //submit the form and redirect to main post page
     @PostMapping("/posts/create")
     public String createNewPost(@ModelAttribute Post newPost) {
         Post savedPost = postDao.save(newPost);
         System.out.println(savedPost.getId());
 
-        return "redirect:/posts/" + savedPost.getId();
+        return "redirect:/posts/";
     }
 }
